c0531af7ac10cace953b7cdd9032c61eb3d82bef
Gautam-Balachandran <gautam.balachandran@gmail.com>
2024-08-06 09:57:44 -0400

    BFS-4 Completed

---
+++ Minesweeper.py
@@ -0,0 +1,93 @@
+﻿# Time Complexity : O(m*n), where m is the number of rows and n is the number
+# of columns of the board
+# Space Complexity : O(m*n)
+from collections import deque
+
+class Solution:
+    def updateBoard(self, board, click):
+        DIRECTIONS = [(-1, -1), (-1, 0), (-1, 1),  # Upper-Left, Up, Upper-Right
+            (0, -1),           (0, 1),   # Left, Right
+            (1, -1),  (1, 0),  (1, 1)    # Bottom-Left, Bottom, Bottom-Right
+        ]
+        
+        row, col = click
+
+        # If a mine is clicked, game over
+        if board[row][col] == 'M':
+            board[row][col] = 'X'
+            return board
+
+        # BFS initialization
+        queue = deque([(row, col)])
+
+        while queue:
+            r, c = queue.popleft()
+            mine_count = 0
+
+            # Count mines in adjacent cells
+            for dr, dc in DIRECTIONS:
+                nr, nc = r + dr, c + dc
+                if 0 <= nr < len(board) and 0 <= nc < len(board[0]):
+                    if board[nr][nc] == 'M':
+                        mine_count += 1
+
+            if mine_count > 0:
+                # If there are adjacent mines, mark the cell with the number of
+                # mines
+                board[r][c] = str(mine_count)
+            else:
+                # If no adjacent mines, mark as 'B' and add all adjacent 'E'
+                # cells to the queue
+                board[r][c] = 'B'
+                for dr, dc in DIRECTIONS:
+                    nr, nc = r + dr, c + dc
+                    if 0 <= nr < len(board) and 0 <= nc < len(board[0]) and board[nr][nc] == 'E':
+                        board[nr][nc] = 'B'  # Mark as visited
+                        queue.append((nr, nc))
+
+        return board
+
+# Example 1
+board1 = [['E', 'E', 'E', 'E', 'E'],
+    ['E', 'E', 'M', 'E', 'E'],
+    ['E', 'E', 'E', 'E', 'E'],
+    ['E', 'E', 'E', 'E', 'E']]
+click1 = [3, 0]
+print(Solution().updateBoard(board1, click1))
+# Expected Output:
+# [
+#     ['B', '1', 'E', '1', 'B'],
+#     ['B', '1', 'M', '1', 'B'],
+#     ['B', '1', '1', '1', 'B'],
+#     ['B', 'B', 'B', 'B', 'B']
+# ]
+
+# Example 2
+board2 = [['B', '1', 'E', '1', 'B'],
+    ['B', '1', 'M', '1', 'B'],
+    ['B', '1', '1', '1', 'B'],
+    ['B', 'B', 'B', 'B', 'B']]
+click2 = [1, 2]
+print(Solution().updateBoard(board2, click2))
+# Expected Output:
+# [
+#     ['B', '1', 'E', '1', 'B'],
+#     ['B', '1', 'X', '1', 'B'],
+#     ['B', '1', '1', '1', 'B'],
+#     ['B', 'B', 'B', 'B', 'B']
+# ]
+
+# Example 3
+board3 = [['E', 'E', 'E', 'E'],
+    ['E', 'M', 'M', 'E'],
+    ['E', 'E', 'E', 'E'],
+    ['E', 'E', 'E', 'E']]
+click3 = [1, 1]
+print(Solution().updateBoard(board3, click3))
+# Expected Output:
+# [
+#     ['E', 'E', 'E', 'E'],
+#     ['E', 'X', 'M', 'E'],
+#     ['E', 'E', 'E', 'E'],
+#     ['E', 'E', 'E', 'E']
+# ]
---
+++ SnakesAndLadders.py
@@ -0,0 +1,73 @@
+﻿# Time Complexity : O(n^2)
+# Space Complexity : O(n^2)
+from collections import deque
+
+class Solution:
+    def snakesAndLadders(self, board):
+        if not board or not board[0]:
+            return 0
+
+        n = len(board)
+        moves = [-1] * (n * n + 1)  # 1D representation of the board
+        queue = deque([1])  # BFS queue
+        moves[1] = -2  # Mark the first cell as visited
+        steps = 0
+        idx = 1
+        left_to_right = True
+
+        # Flatten 2D array
+        for i in range(n - 1, -1, -1):
+            if left_to_right:
+                for j in range(n):
+                    moves[idx] = board[i][j]
+                    idx += 1
+            else:
+                for j in range(n - 1, -1, -1):
+                    moves[idx] = board[i][j]
+                    idx += 1
+            left_to_right = not left_to_right
+
+        # Perform BFS
+        while queue:
+            size = len(queue)
+            for _ in range(size):
+                curr = queue.popleft()
+                if curr == n * n:
+                    return steps
+                for j in range(1, 7):
+                    next_pos = curr + j
+                    if next_pos <= n * n and moves[next_pos] != -2:
+                        if moves[next_pos] != -1:
+                            queue.append(moves[next_pos])
+                        else:
+                            queue.append(next_pos) # Snake or Ladder
+                        moves[next_pos] = -2  # Mark as visited
+            steps += 1
+
+        return -1
+
+# Example 1
+board1 = [[-1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1],
+    [-1, 35, -1, -1, 13, -1],
+    [-1, -1, -1, -1, -1, -1],
+    [-1, 15, -1, -1, -1, -1]]
+print(Solution().snakesAndLadders(board1))  # Output: 4
+
+# Example 2
+board2 = [[-1, -1, -1, -1, 48, 5, -1],
+    [12, 29, 13, 9, -1, 2, 32],
+    [-1, -1, 21, -1, -1, -1, -1],
+    [42, 37, 21, -1, -1, 26, -1],
+    [28, -1, 26, 10, -1, -1, 36],
+    [22, 33, 18, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1]]
+print(Solution().snakesAndLadders(board2))  # Output: 5
+
+# Example 3
+board3 = [[-1, -1, -1, -1],
+    [14, -1, 11, -1],
+    [-1, -1, -1, -1],
+    [-1, -1, -1, 15]]
+print(Solution().snakesAndLadders(board3))  # Output: 2
